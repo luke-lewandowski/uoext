2aad327b7473e4765ca2208187a09228c0afc60f
Luke Lewandowski <luke@lewandowskis.com>
2014-05-18 18:57:35 +1000

    Added Journal V2 library. (http://www.easyuo.com/forum/viewtopic.php?t=47299)

--- uoext.lua
+++ uoext.lua
@@ -6,6 +6,7 @@ dofile(".\\Utils\\Config.lua")
 dofile(".\\Lib\\FluentUO\\FluentUO.lua")
 dofile(".\\Lib\\GetHitBarLife\\GetHitBarLife.lua")
 dofile(".\\Lib\\json4lua\\json4lua.lua")
+dofile(".\\Lib\\journal_v2\\journal_v2.lua")
 
 -- Structures
 dofile(".\\Structs\\LimitedStack.lua")
